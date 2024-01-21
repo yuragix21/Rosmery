7f95d25891d2ca74286a5ab5566537a442ce7bdb
torviscoadri20 <torviscoadri20@gmail.com>
2024-01-21 18:29:31 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+window.onload = function(){
+    var mensaje = "Presiona la pantalla para buscar lo que siento por ti";
+    alert(mensaje);
+};
+
+
+var miMusica = document.querySelector('audio');
+
+window.addEventListener('load', function(){
+    miMusica.play();
+});
